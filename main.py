d7b93c3d60e59264df02c950b9722c653ea2af52
Keyon Jerome <keyonjerome@gmail.com>
2019-01-23 06:23:44 -0500

    Added tons of comments, can now get distance from object, new HSV values.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+# imports
 import math
-
 import cv2
 import numpy as np
 import threading
 from networktables import NetworkTables
 
+# Robot networking code, makes program wait until network connection is confirmed to continue
 '''
 cond = threading.Condition()
 notified = [False]
@@ -28,13 +29,35 @@ with cond:
 print("Connected!")
 '''
 
+# print out every item in an array, instead of using an ellipsis to shorten it.
 np.set_printoptions(threshold=np.inf)
-# define the HSV threshold ranges.
-hue_range = [28, 85]
-saturation_range = [23, 255]
-value_range = [96, 255]
 
+# define the HSV threshold ranges for the field retroreflective tape.
+# vision tape green on table lighting values
+#hue_range = [28, 85]
+#saturation_range = [23, 255]
+#value_range = [96, 255]
+# keyon's room lighting values
+#hue_range = [32,85]
+#saturation_range = [0,40]
+#value_range = [248,255]
+# values used during calculation of focal length, for reference
+hue_range = [66,76]
+saturation_range = [202,255]
+value_range = [213,255]
+# ignore any detected object with a perimeter less than this.
+perimeter_threshold = 100
 
+# calculated with 30 cm data using F = (P x D) / W
+# focal length of the Lifecam 3000
+focal_length = 360.4782502
+# width of the tape in centimetres
+width_of_tape = 12.7
+
+# The BGR (RGB but backwards because OpenCV handles it that way) colorspace is not good for isolating based on color.
+# This is because the color is a combination of three different slots: blue, green, and red.
+# The HSV colorspace refers to hue, saturation, and value. Hue describes the actual colour of the pixel.
+# Since all the colour is in just one slot, it's a thousand times easier to isolate for colour using HSV.
 def hsv_threshold(input, hue, saturation, value):
     """Segment an image based on hue, saturation, and value ranges.
     Args:
@@ -45,6 +68,7 @@ def hsv_threshold(input, hue, saturation, value):
     Returns:
         A black and white numpy.ndarray.
     """
+    # Convert the image to HSV
     out = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
 
     # this function goes through the input numpy.ndarray (the image)
@@ -53,42 +77,94 @@ def hsv_threshold(input, hue, saturation, value):
     return cv2.inRange(out, (hue[0], saturation[0], value[0]), (hue[1], saturation[1], value[1]))
 
 # https://stackoverflow.com/questions/48109650/how-to-detect-two-different-colors-using-cv2-inrange-in-python-opencv
+# draw the corners of the detected object. Takes in a cv2.boxPoints(rect) result
+def drawCorners(points):
+    # bottom right: points[0]
+    cv2.circle(objects, (points[0][0], points[0][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
+    # bottom left: points[1]
+    cv2.circle(objects, (points[1][0], points[1][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
+    # top left: points[2]
+    cv2.circle(objects, (points[2][0], points[2][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
+    # top right: points[3]
+    cv2.circle(objects, (points[3][0], points[3][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
 
+def getDistanceToCamera(minAreaRect,knownWidth, knownFocal, widthPixels):
+    distance = (knownWidth*knownFocal)/widthPixels
+    return distance
+# boolean logic to check if the detected object is the retroreflective tape.
+def checkIfFound(check_perimeter, check_area, check_angle,check_height,check_width,):
+    #print("Perimeter:", check_perimeter,"\nArea:", check_area,"\nAngle:", check_angle)
+    #print("Width:",check_width, "\nHeight:",check_height)
+    # avoid division by zero
+    if check_width > 0 and check_height > 0:
+    #    print(check_height/check_width)
+        # find the ratio between height and width. As the rectangle is 5 by 2 inches, this should always be 2.5
+        # But use this range to allow for inconsistency.
+        # For the angles of the rect, it should be -78 and -12. This makes sense because OpenCV thinks of the box as on
+        # a cartesian grid, turning counterclockwise. Why are both negative? Because the points OpenCV thinks is the
+        # top left changes depending on the orientation (and therefore the angle!) of the rectangle.
+        # For example, it might assign points[0] as the top left when really it should be points[2] how we see it.
+        if (2.0 < check_height/check_width < 3) and (-16 < angle < -8 or 8 < angle < 16 ):
+     #       print("FOUND!")
+            return True
+        else:
+            return False
+# define the video camera (port 0)
 cap = cv2.VideoCapture(0)
+
+# "test" and other boolean logic is only here to quickly switch between taking in a single image vs.
+# parsing an entire video feed.
 test = True
 while test:
     # test = False
+    # take a frame from the video capture
     ret, bgr_img = cap.read()
     # read in the image
     # bgr_img = cv2.imread('testimages/retroreflectivetapegreen.jpg')
 
-    # resize the image to 300x300, 0.5 interpolation if (300,300) does not work correctly
-    imageResized = cv2.resize(bgr_img, (300, 300), fx=0.5, fy=0.5)
+    # resize the image to half its original size
+    imageResized = cv2.resize(bgr_img, (0, 0), fx=0.5, fy=0.5)
 
-    # Gaussian blur the image.
+    # Gaussian blur the image, (191,191) refers to how much the image is being blurred on the X and Y axes.
     blurred_image = cv2.GaussianBlur(imageResized, (191, 191), 1)
     # cv2.imshow("BLURRED", blurred_image)
 
     # define the erosion kernel and erode the image
     # eroding eliminates inconsistencies between pixels, like small flares or bits
+    # More iterations means a cleaner image, but takes a lot of processing power.
     kernel = np.ones((5, 5), 'uint8')
-    erode = cv2.erode(blurred_image, kernel, iterations=1)
+    erode = cv2.erode(blurred_image, kernel, iterations=3)
 
     # Run an HSV threshold on the image to isolate the retroreflective tape.
     thresholded_image = hsv_threshold(blurred_image, hue_range, saturation_range, value_range)
     # print(thresholded_image)
 
+    # hierarchy refers to the tree of contours: whether a contour has contours inside it, etc.
+    # contours is an array of the outsides of any detected object.
+    # For example, contours[0] could be the outsides of a rectangle, while contours[1] could be a circle...
+    # The x and y co-ordinates of every point on the contours is given as well.
+    # by using cv2.RETR_EXTERNAL, we only take the top-level contours; no contours inside of contours, etc.
     contours, hierarchy = cv2.findContours(thresholded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     index = -1
     thickness = 4
     color = (255, 0, 255)
-
+    # create an empty array (array of 0s) to draw the contours onto later.
+    # An image in computer vision is just a 2D array with 3 channels: Think of a cartesian grid, but
+    # with 3 values inside each point (for the pixel's colour)
+    # Ex: [0][0] would mean the top-left corner of the image, and when you reference image[0][0]:
+    # You get [blue,green,red], because image[0][0] is a pixel and [blue,green,red] are its values.
+    # image.shape[0] is the height of the image (how many pixels)
+    # image.shape[0] is the width of the image
     objects = np.zeros([thresholded_image.shape[0], thresholded_image.shape[1], 3], 'uint8')
+    # loop through every object with contours
     for c in contours:
+        # calculate the area of the object
         area = cv2.contourArea(c)
+        # calculate the perimeter of the object
         perimeter = cv2.arcLength(c, True)
-        if perimeter > 100:
+
+        if perimeter > perimeter_threshold:
             cv2.drawContours(objects, [c], -1, color, 1)
             # draw contours of the thresholded image one by one, do not do the inner fill of the image
             M = cv2.moments(c)
@@ -99,50 +175,69 @@ while test:
             # cv2.circle(objects, (cx,cy), 4, (0,0,255),-1)
 
             # print("Area: {}, perimeter: {}".format(area,perimeter))
-            print(contours)
+            #print(contours)
             rect = cv2.minAreaRect(c)
             # get rectangle information
             # https://stackoverflow.com/questions/36293335/using-bounding-rectangle-to-get-rotation-angle-not-working-opencv-python
             center = rect[0]
             angle = rect[2]
-            rot = cv2.getRotationMatrix2D(center, angle - 90, 1)
-            print(angle)
+            #rot = cv2.getRotationMatrix2D(center, angle - 90, 1)
+            #print("Angle: ",angle)
             # img = cv2.warpAffine(img, rot, (rows, cols))
 
+            # this converts the given rect to the main 4 corners of the rect
             box = cv2.boxPoints(rect)
             points = cv2.boxPoints(rect)
-            # widthOfBox = sqrt(points[0][0])
-
+            #  modify the data type of the array, convert to integers
             box = np.int0(box)
+            # draw the contours of the box
             cv2.drawContours(objects, [box], 0, (0, 0, 255), 2)
 
+            # define the 4 corners of the rectangle
             bottom_left = points[1]
             bottom_right = points[0]
             top_left = points[2]
             top_right = points[3]
-            width_in_pixels = math.sqrt((bottom_left[0] - bottom_right[1]) ** 2 + (bottom_left[1] - bottom_right[1]) ** 2)
-            print(bottom_left, bottom_right)
-            print(width_in_pixels)
+            # use cartesian grid math to figure out the width and height of the rectangles:
+            # distance = sqrt((x1-x2)^2 + (y1-y2)^2)
+            width_in_pixels = math.sqrt((bottom_left[0] - bottom_right[0]) ** 2 + (bottom_left[1] - bottom_right[1]) ** 2)
+            height_in_pixels = math.sqrt((bottom_left[0]-top_left[0])**2 + (bottom_left[1]-top_left[1])**2)
 
-            # bottom right: points[0]
-            cv2.circle(objects, (points[0][0], points[0][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
-            # bottom left: points[1]
-            cv2.circle(objects, (points[1][0], points[1][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
-            # top left: points[2]
-            cv2.circle(objects, (points[2][0], points[2][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
-            # top right: points[3]
-            cv2.circle(objects, (points[3][0], points[3][1]), 20, (255, 0, 0), thickness=1, lineType=8, shift=0)
+            # Depending on whether the rectangle is being laid flat (imagine the x-axis) vs. being laid out upwards (y-axis),
+            # OpenCV will switch between width and height as it'll start counting from a different corner.
+            # If the angle given by OpenCV is negative, then the rectangle is laid out flat, and width will be calculated as if it's height.
+            # Switch the two if this happens!
+            if angle < 0:
+                switch = height_in_pixels
+                height_in_pixels = switch
+                height_in_pixels = width_in_pixels
+                width_in_pixels = switch
 
-    # corners = cv2.goodFeaturesToTrack(rec,4,0.01,10)
+            # print out data
+            print("Bottom left and bottom right (used to calc width): ", bottom_left, bottom_right)
+            print("Bottom left and top left (used to calc height): ", bottom_left, top_left)
+            print("Width: ", width_in_pixels)
+            print("Height: ", height_in_pixels)
+            print("Area: ", area)
+            print("Angle:", angle)
+            print("Perimeter:",perimeter)
+            print("All four corners: ")
+            print("Bottom left:",bottom_left)
+            print("Bottom right:",bottom_right)
+            print("Top left:",top_left)
+            print("Top right:",top_right)
 
-    # corners = np.int0(corners)
-    # for corner in corners:
-    #   x,y = corner.ravel()
-    #   cv2.circle(thresholded_image,(x,y),90,30,-1)
+            # draw the corners of the rectangle and check if it's the field's vision target
+            drawCorners(points)
+            checkIfFound(perimeter,area,angle,height_in_pixels,width_in_pixels)
+            print("Distance:",getDistanceToCamera(rect,width_of_tape,focal_length,width_in_pixels))
+            print("\n")
 
-    givenKey = cv2.waitKey(200)
-    #   if givenKey == ord('x'):
-    #       break
+    # Wait 10 seconds between iterations.
+    givenKey = cv2.waitKey(10)
+    # Break if
+    if givenKey == ord('x'):
+        break
 
     cv2.imshow("Image", thresholded_image)
     cv2.imshow("Objects", objects)
