08140db5faa3b597b83c2afb35e4baf65793ab5e
Keyon-Jerome <keyonjerome@gmail.com>
2019-10-22 07:48:49 -0400

    Print out values propery and get distance.

--- main.py
+++ main.py
@@ -276,9 +276,10 @@ with open('output.json') as json_file:
 
                 print("RETVAL,RVEC,TVEC:")
                 print(retval)
-                print(rvec)
-                print(tvec) 
+                print(rvec.get())
+                print(tvec.get()) 
                 print()
+                d = math.sqrt(tx*tx + ty*ty + tz*tz)
 
                 #  modify the data type of the array, convert to integers
                 box = np.int0(box)
